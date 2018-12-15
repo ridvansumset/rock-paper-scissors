3908ef67bccdcba7edb36670d49bd791b8c17251
ridvansumset <ridvansumset@gmail.com>
2018-12-15 16:28:04 +0300

    update README, better var names

---
+++ README.md
@@ -0,0 +1,19 @@
+# Rock Paper Scissors Game
+
+Items on the left is yours to choose and the ones on the right belong to AI.
+
+Just click one of your items and AI will respond immediately.
+
+## How to Get & Run
+
+Go to command line and run command below:
+
+`git clone git@github.com:ridvansumset/rock-paper-scissors.git`
+
+If you didn't do SSH configuration do:
+
+`git clone https://github.com/ridvansumset/rock-paper-scissors.git`
+
+Or you can download as zip by clicking 'Clone or download' button on this page.
+
+Then, open `index.html` file in your browser and you are ready to play!
--- main.js
+++ main.js
@@ -1,27 +1,21 @@
-	// items' values for user
-	const rock = 0;
-	const paper = 1;
-	const scissor = 2;
+// values of items for user
+const rock = 0, paper = 1, scissor = 2;
 
-	const user0 = document.getElementById('user0');
-	const user1 = document.getElementById('user1');
-	const user2 = document.getElementById('user2');
+const user0 = document.getElementById('user0');
+const user1 = document.getElementById('user1');
+const user2 = document.getElementById('user2');
 
-	const userWinnings = document.querySelector('.userWinnings');
-	const aiWinnings = document.querySelector('.aiWinnings');
-	const tieWinnings = document.querySelector('.tieWinnings');
+const userWinnings = document.querySelector('.userWinnings');
+const aiWinnings = document.querySelector('.aiWinnings');
+const tieWinnings = document.querySelector('.tieWinnings');
 
-	// items' values for AI when user makes a selection
-	const rockArray = [0, 1, -1];
-	const paperArray = [0, 1, 2];
-	const scissorArray = [3, 1, 2];
+// values of items for AI when user makes a selection
+const arrayForRock = [0, 1, -1];
+const arrayForPaper = [0, 1, 2];
+const arrayForScissor = [3, 1, 2];
 
-	var user_wins = 0;
-	var ai_wins = 0;
-	var ties = 0;
-
-	var selected_item_val = 4; // initial value (out of range in purpose) 
-
+var user_win_count = 0, ai_win_count = 0, tie_count = 0;
+var selected_item_val = 4; // initial value (out of range in purpose)
 
 user0.addEventListener('click', function() {
 	selected_item_val = rock; // e.g selected item's val is 0
@@ -49,7 +43,7 @@ function showUserSelection(userIndex, userWon, tie = false) {
 			style = 'winner';
 		}
 	}
-	
+
 	for (let i = 0; i < 3; i++) {
 		document.getElementById('user' + i).classList.remove('winner');
 		document.getElementById('user' + i).classList.remove('loser');
@@ -70,7 +64,7 @@ function showAiSelection(aiIndex, aiWon, tie = false) {
 			style = 'winner';
 		}
 	}
-	
+
 	for (let i = 0; i < 3; i++) {
 		document.getElementById(i).classList.remove('winner');
 		document.getElementById(i).classList.remove('loser');
@@ -83,21 +77,21 @@ function showAiSelection(aiIndex, aiWon, tie = false) {
 
 function highlight(winning) {
 	if (winning === 'user') {
-		user_wins += 1;
-		userWinnings.textContent = 'You win: ' + user_wins;
+		user_win_count += 1;
+		userWinnings.textContent = 'You win: ' + user_win_count;
 		userWinnings.style.backgroundColor = 'lightgreen';
 		aiWinnings.style.backgroundColor = 'white';
 		tieWinnings.style.backgroundColor = 'white';
 	} else if (winning === 'ai') {
-		ai_wins += 1;
-		aiWinnings.textContent = 'AI wins: ' + ai_wins;
-		aiWinnings.style.backgroundColor = 'lightgreen';
+		ai_win_count += 1;
+		aiWinnings.textContent = 'AI wins: ' + ai_win_count;
+		aiWinnings.style.backgroundColor = 'lightcoral';
 		userWinnings.style.backgroundColor = 'white';
 		tieWinnings.style.backgroundColor = 'white';
 	} else if (winning === 'tie') {
-		ties += 1;
-		tieWinnings.textContent = 'Tie : ' + ties;
-		tieWinnings.style.backgroundColor = 'lightgreen';
+		tie_count += 1;
+		tieWinnings.textContent = 'Tie : ' + tie_count;
+		tieWinnings.style.backgroundColor = 'lightgrey';
 		userWinnings.style.backgroundColor = 'white';
 		aiWinnings.style.backgroundColor = 'white';
 	} else {
@@ -107,7 +101,6 @@ function highlight(winning) {
 
 function whoIsWinner(userVal, aiVal, aiIndex) {
 	if (userVal > aiVal) {
-			
 			highlight('user');
 			showUserSelection(userVal, true);
 			showAiSelection(aiIndex, false);
@@ -125,18 +118,15 @@ function whoIsWinner(userVal, aiVal, aiIndex) {
 function select(itemVal) {
 	if (itemVal === rock) {
 		let rand = Math.floor(Math.random() * 3) - 1;
-		let index = rockArray.indexOf(rand);
-
+		let index = arrayForRock.indexOf(rand);
 		whoIsWinner(itemVal, rand, index);
 	} else if (itemVal === paper) {
 		let rand = Math.floor(Math.random() * 3);
-		let index = paperArray.indexOf(rand);
-
+		let index = arrayForPaper.indexOf(rand);
 		whoIsWinner(itemVal, rand, index);
 	} else if (itemVal === scissor) {
 		let rand = Math.floor(Math.random() * 3) + 1;
-		let index = scissorArray.indexOf(rand);
-
+		let index = arrayForScissor.indexOf(rand);
 		whoIsWinner(itemVal, rand, index);
 	} else {
 		alert('There is a problem!');
