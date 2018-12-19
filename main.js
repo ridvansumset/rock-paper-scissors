e0ea780918116decbee24f2f105eb1bd64f5af71
ridvansumset <ridvansumset@gmail.com>
2018-12-19 13:58:32 +0300

    remove semicolons

--- main.js
+++ main.js
@@ -1,60 +1,60 @@
-const rock = 0, paper = 1, scissor = 2;
-const user0 = document.getElementById('user0'), user1 = document.getElementById('user1'), user2 = document.getElementById('user2');
-const userWins = document.querySelector('.userWins'), aiWins = document.querySelector('.aiWins'), tieWins = document.querySelector('.tieWins');
-var user_win_count = 0, ai_win_count = 0, tie_count = 0;
+const rock = 0, paper = 1, scissor = 2
+const user0 = document.getElementById('user0'), user1 = document.getElementById('user1'), user2 = document.getElementById('user2')
+const userWins = document.querySelector('.userWins'), aiWins = document.querySelector('.aiWins'), tieWins = document.querySelector('.tieWins')
+var user_win_count = 0, ai_win_count = 0, tie_count = 0
 
-user0.addEventListener('click', () => getUserSelection(rock));
-user1.addEventListener('click', () => getUserSelection(paper));
-user2.addEventListener('click', () => getUserSelection(scissor));
+user0.addEventListener('click', () => getUserSelection(rock))
+user1.addEventListener('click', () => getUserSelection(paper))
+user2.addEventListener('click', () => getUserSelection(scissor))
 
 function showUserAndAISelectionsThenHighlightScoreboard(userIndex, userWon, aiIndex, tie = false) {
 	[].forEach.call(document.querySelectorAll('.item, .score-item'), function(element) {
-		element.classList.contains('score-item') ? element.style.backgroundColor = 'white' : element.classList.remove('winner', 'loser', 'tie');
-	});
+		element.classList.contains('score-item') ? element.style.backgroundColor = 'white' : element.classList.remove('winner', 'loser', 'tie')
+	})
 	if (tie) {
-		[].forEach.call(document.querySelectorAll('#user' + userIndex + ', ' + '#ai' + aiIndex), function(element) { element.classList.add('tie') });
-		tieWins.textContent = 'Tie : ' + (++tie_count);
-		tieWins.style.backgroundColor = 'lightgrey';
-		return 'It\'s a Tie!';
+		[].forEach.call(document.querySelectorAll('#user' + userIndex + ', ' + '#ai' + aiIndex), function(element) { element.classList.add('tie') })
+		tieWins.textContent = 'Tie : ' + (++tie_count)
+		tieWins.style.backgroundColor = 'lightgrey'
+		return 'It\'s a Tie!'
 	} else if (userWon) {
-		document.getElementById('user' + userIndex).classList.add('winner');
-		document.getElementById('ai' + aiIndex).classList.add('loser');
-		userWins.textContent = 'You win: ' + (++user_win_count);
-		userWins.style.backgroundColor = 'lightgreen';
-		return 'User beats AI!';
+		document.getElementById('user' + userIndex).classList.add('winner')
+		document.getElementById('ai' + aiIndex).classList.add('loser')
+		userWins.textContent = 'You win: ' + (++user_win_count)
+		userWins.style.backgroundColor = 'lightgreen'
+		return 'User beats AI!'
 	} else {
-		document.getElementById('user' + userIndex).classList.add('loser');
-		document.getElementById('ai' + aiIndex).classList.add('winner');
-		aiWins.textContent = 'AI wins: ' + (++ai_win_count);
-		aiWins.style.backgroundColor = 'lightcoral';
-		return 'AI beats User!';
+		document.getElementById('user' + userIndex).classList.add('loser')
+		document.getElementById('ai' + aiIndex).classList.add('winner')
+		aiWins.textContent = 'AI wins: ' + (++ai_win_count)
+		aiWins.style.backgroundColor = 'lightcoral'
+		return 'AI beats User!'
 	}
 }
 
 function getWinner(userIndex, aiVal, aiIndex) {
 	if (userIndex > aiVal) {
-			return showUserAndAISelectionsThenHighlightScoreboard(userIndex, true, aiIndex);
+			return showUserAndAISelectionsThenHighlightScoreboard(userIndex, true, aiIndex)
 		} else if (userIndex < aiVal) {
-			return showUserAndAISelectionsThenHighlightScoreboard(userIndex, false, aiIndex);
+			return showUserAndAISelectionsThenHighlightScoreboard(userIndex, false, aiIndex)
 		} else {
-			return showUserAndAISelectionsThenHighlightScoreboard(userIndex, false, aiIndex, true);
+			return showUserAndAISelectionsThenHighlightScoreboard(userIndex, false, aiIndex, true)
 		}
 }
 
 function getUserSelection(itemVal) {
-	var returnArray = [];
+	var returnArray = []
 	if (itemVal === rock) {
-		let rand = Math.floor(Math.random() * 3) - 1;
-		returnArray = [0, 1, -1];
-		getWinner(itemVal, rand, returnArray.indexOf(rand));
+		let rand = Math.floor(Math.random() * 3) - 1
+		returnArray = [0, 1, -1]
+		getWinner(itemVal, rand, returnArray.indexOf(rand))
 	} else if (itemVal === paper) {
-		let rand = Math.floor(Math.random() * 3);
-		returnArray = [0, 1, 2];
-		getWinner(itemVal, rand, returnArray.indexOf(rand));
+		let rand = Math.floor(Math.random() * 3)
+		returnArray = [0, 1, 2]
+		getWinner(itemVal, rand, returnArray.indexOf(rand))
 	} else if (itemVal === scissor) {
-		let rand = Math.floor(Math.random() * 3) + 1;
-		returnArray = [3, 1, 2];
-		getWinner(itemVal, rand, returnArray.indexOf(rand));
+		let rand = Math.floor(Math.random() * 3) + 1
+		returnArray = [3, 1, 2]
+		getWinner(itemVal, rand, returnArray.indexOf(rand))
 	}
-	return returnArray;
+	return returnArray
 }
--- test.js
+++ test.js
@@ -1,54 +1,54 @@
 if (typeof module !== 'undefined' && module.exports) {
-  var QUnit = require('qunitjs');
-  var test = QUnit.test;
-  require('qunit-tap')(QUnit, console.log);
+  var QUnit = require('qunitjs')
+  var test = QUnit.test
+  require('qunit-tap')(QUnit, console.log)
 
-  var getWinner = require('./main.js');
-  var getUserSelection = require('./main.js');
+  var getWinner = require('./main.js')
+  var getUserSelection = require('./main.js')
 }
 
 // getWinner tests
 test('getWinner(2, 1, 1) should return \"User beats AI!\"', function(assert) {
-  var result = getWinner(2, 1, 1);
-  var expected = 'User beats AI!';
-  assert.deepEqual(result, expected);
-});
+  var result = getWinner(2, 1, 1)
+  var expected = 'User beats AI!'
+  assert.deepEqual(result, expected)
+})
 
 test('getWinner(0, 1, 1) should return \"AI beats User!\"', function(assert) {
-  var result = getWinner(0, 1, 1);
-  var expected = 'AI beats User!';
-  assert.deepEqual(result, expected);
-});
+  var result = getWinner(0, 1, 1)
+  var expected = 'AI beats User!'
+  assert.deepEqual(result, expected)
+})
 
 test('getWinner(1, 1, 1) should return \"It\'s a Tie!\"', function(assert) {
-  var result = getWinner(1, 1, 1);
-  var expected = 'It\'s a Tie!';
-  assert.deepEqual(result, expected);
-});
+  var result = getWinner(1, 1, 1)
+  var expected = 'It\'s a Tie!'
+  assert.deepEqual(result, expected)
+})
 
 // getUserSelection tests
 test('getUserSelection(0) should return [0, 1, -1]', function(assert) {
-  var result = getUserSelection(0);
-  var expected = [0, 1, -1];
-  assert.deepEqual(result, expected);
-});
+  var result = getUserSelection(0)
+  var expected = [0, 1, -1]
+  assert.deepEqual(result, expected)
+})
 
 test('getUserSelection(1) should return [0, 1, 2]', function(assert) {
-  var result = getUserSelection(1);
-  var expected = [0, 1, 2];
-  assert.deepEqual(result, expected);
-});
+  var result = getUserSelection(1)
+  var expected = [0, 1, 2]
+  assert.deepEqual(result, expected)
+})
 
 test('getUserSelection(2) should return [3, 1, 2]', function(assert) {
-  var result = getUserSelection(2);
-  var expected = [3, 1, 2];
-  assert.deepEqual(result, expected);
-});
+  var result = getUserSelection(2)
+  var expected = [3, 1, 2]
+  assert.deepEqual(result, expected)
+})
 
 test('getUserSelection(23) should return []', function(assert) {
-  var result = getUserSelection(23);
-  var expected = [];
-  assert.deepEqual(result, expected);
-});
+  var result = getUserSelection(23)
+  var expected = []
+  assert.deepEqual(result, expected)
+})
 
-if (typeof module !== 'undefined' && module.exports) { QUnit.load(); } // run the tests
+if (typeof module !== 'undefined' && module.exports) { QUnit.load() } // run the tests
